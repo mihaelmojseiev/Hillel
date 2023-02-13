b7bdef981cc38d5b0cd3bc3f645fb43c4980014b
mihaelmojseiev <mihael.mojseiev@ab-soft.net>
2023-02-13 10:30:55 +0100

    10*30

--- homework_6/task_4.py
+++ homework_6/task_4.py
@@ -1,4 +1,18 @@
-# Завдання 4.
-# Написати функцію, що відповідає на питання, чи перетинають два заданих кола на площині. Кожне коло задається координатами центра та радіусом.
-# def circles_intersect(x1, y1, r1, x2, y2, r2): # returns boolean value
-#     pass
+import math
+
+
+def circles_intersect(x1, y1, r1, x2, y2, r2):
+    d = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    if d > r1 + r2:
+        print(True)
+    elif d < r2 - r1:
+        print(True)
+    elif d > r1 + r2:
+        print(True)
+    else:
+        print(False)
+
+
+x1, y1, r1, x2, y2, r2 = map(int, input("Введи 6 значений: ").split())
+
+circles_intersect(x1, y1, r1, x2, y2, r2)
