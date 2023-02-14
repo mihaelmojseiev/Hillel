1dbfa2b3616c16c66d1c7e4085749a899328949e
mihaelmojseiev <mihael.mojseiev@ab-soft.net>
2023-02-14 22:15:42 +0100

    1622

--- homework_6/task_7.py
+++ homework_6/task_7.py
@@ -1,8 +1,14 @@
 def fibonacci(n):
-    if (n <= 1):
-        return n
-    else:
-        return (fibonacci(n - 1) + fibonacci(n - 2))
+   if n <= 1:
+       return n
+   else:
+       return(fibonacci(n-1) + fibonacci(n-2))
 
+numb = (int(input('Введи номер: ')))
 
-print(fibonacci(int(input('Введи номер: '))))
+if numb <= 0:
+   print("Ввёл отрицательное")
+else:
+   print("Последовательность такая:")
+   for i in range(numb):
+       print(fibonacci(i))
